cdccebc1a4a896e8a3286729cef70d0e96bf77a4
Ian Miell <ian.miell@gmail.com>
2015-01-16 10:36:08 +0000

    latest

--- cluster_setup/coreos_do_setup.py
+++ cluster_setup/coreos_do_setup.py
@@ -18,26 +18,31 @@ class coreos_do_setup(ShutItModule):
 		discovery = shutit.get_output().strip()
 		if shutit.cfg[self.module_id]['ssh_key_id'] == '':
 			shutit.send("""curl -s -X GET -H 'Content-Type: application/json' -u "${TOKEN}:" "https://api.digitalocean.com/v2/account/keys" | jq -M '.ssh_keys[0].id'""")
-			ssh_key = shutit.get_output().strip()
+			ssh_key_id = shutit.get_output().strip()
 		else:
-			ssh_key = shutit.cfg[self.module_id]['ssh_key_id']
+			ssh_key_id = shutit.cfg[self.module_id]['ssh_key_id']
 		# ssh keys
 		shutit.send('mkdir -p /root/.ssh')
 		shutit.send_host_file('/root/.ssh/' + shutit.cfg[self.module_id]['ssh_key_filename'],shutit.cfg[self.module_id]['ssh_key_file'])
 		shutit.send('chmod 0600 /root/.ssh/' + shutit.cfg[self.module_id]['ssh_key_filename'])
+		shutit.send('eval `ssh-agent -s`')
+		shutit.send('ssh-add ~/.ssh/id_rsa')
 		shutit.cfg[self.module_id]['created_droplets'] = []
 		for machine in range(1,int(shutit.cfg[self.module_id]['num_machines']) + 1):
 			cloud_config = open('context/cloud-config').read().strip()
 			cloud_config = string.replace(cloud_config,'DISCOVERY',discovery)
-			command = '''curl -s -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" -d '{"name":"coreos-''' + str(machine) + '''","region":"nyc3","size":"512mb","image":"coreos-stable","ssh_keys":["''' + ssh_key + '''"],"backups":false,"ipv6":true,"user_data":"''' + cloud_config + '''","private_networking":true}' "https://api.digitalocean.com/v2/droplets"'''
+			command = '''curl -s -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" -d '{"name":"coreos-''' + str(machine) + '''","region":"nyc3","size":"512mb","image":"coreos-stable","ssh_keys":["''' + ssh_key_id + '''"],"backups":false,"ipv6":true,"user_data":"''' + cloud_config + '''","private_networking":true}' "https://api.digitalocean.com/v2/droplets"'''
 			shutit.send_file('/tmp/cmd.sh',command)
 			shutit.send('sh /tmp/cmd.sh | jq ".droplet.id" -M')
 			droplet_id = shutit.get_output().strip()
 			shutit.send('rm -f /tmp/cmd.sh')
 			shutit.send('sleep 60 # Wait a decent amount of time; this seems to be required',timeout=180)
 			shutit.send("""curl -s -X GET -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets/""" + droplet_id + '''" | jq -M '.droplet.networks.v4[] | select(.type == "public") | .ip_address''' + "'")
-			ip = shutit.get_output().strip().strip('"')
-			shutit.cfg['build']['report_final_messages'] += 'droplet_id: ' + droplet_id + ': ip address: ' + ip + '\nLog in with: ssh core@' + ip + '\n'
+			public_ip = shutit.get_output().strip().strip('"')
+			shutit.send("""curl -s -X GET -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" "https://api.digitalocean.com/v2/droplets/""" + droplet_id + '''" | jq -M '.droplet.networks.v4[] | select(.type == "private") | .ip_address''' + "'")
+			private_ip = shutit.get_output().strip().strip('"')
+
+			shutit.cfg['build']['report_final_messages'] += 'droplet_id: ' + droplet_id + ': ip address: ' + public_ip + '\nLog in with: ssh core@' + public_ip + '\n'
 			shutit.cfg[self.module_id]['created_droplets'].append({"droplet_id":droplet_id,"public_ip":public_ip,"private_ip":private_ip,"ssh_key_id":ssh_key_id})
 		return True
 
--- kubernetes_setup/kubernetes_setup.py
+++ kubernetes_setup/kubernetes_setup.py
@@ -10,20 +10,33 @@ class kubernetes_setup(ShutItModule):
 
 	def build(self, shutit):
 		#https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-kubernetes-on-top-of-a-coreos-cluster
+		shutit.send('sleep 60 # wait before trying to log in')
+		master_public_ip = shutit.cfg['shutit.tk.coreos_do_setup.coreos_do_setup']['created_droplets'][0]['public_ip']
 		for coreos_machine in shutit.cfg['shutit.tk.coreos_do_setup.coreos_do_setup']['created_droplets']:
 			public_ip = coreos_machine['public_ip']
 			private_ip = coreos_machine['private_ip']
-			shutit.login(command='ssh core@' + public_ip, expect=' ~ ')
-			shutit.send('sudo mkdir -p /opt/shutit')
+			shutit.login(command='ssh -A core@' + public_ip, expect=' ~ ')
+			shutit.send('sudo mkdir -p /opt/bin')
 			shutit.send('docker run -i -t google/golang /bin/bash -c "go get github.com/coreos/flannel"')
 			shutit.send('sudo docker cp $(docker ps -l -q):/gopath/bin/flannel /opt/bin/')
-			shutit.send('cd ~')
-			shutit.send('git clone https://github.com/GoogleCloudPlatform/kubernetes.git')
-			shutit.send('cd kubernetes/build')
-			shutit.send('./release.sh')
-			shutit.send('cd ~/kubernetes/_output/dockerized/bin/linux/amd64')
-			shutit.send('sudo cp * /opt/bin')
 			shutit.logout()
+		# Copy over kub binaries
+		shutit.login(command='ssh -A core@' + master_public_ip, expect=' ~ ')
+		shutit.send('cd ~')
+		shutit.send('git clone https://github.com/GoogleCloudPlatform/kubernetes.git')
+		shutit.send('cd kubernetes/build')
+		shutit.multisend('./release.sh',{'Download it now':'y'})
+		shutit.send('cd ~/kubernetes/_output/dockerized/bin/linux/amd64')
+		shutit.send('sudo cp * /opt/bin')
+		shutit.send('cd /opt/bin')
+		for coreos_machine in shutit.cfg['shutit.tk.coreos_do_setup.coreos_do_setup']['created_droplets']:
+			public_ip = coreos_machine['public_ip']
+			if public_ip == master_public_ip:
+				# No need to do this on master!
+				continue
+			shutit.multisend('tar -czf - . | ssh core@' + public_ip + ' "sudo mkdir -p /opt/bin; cd /opt/bin; sudo tar xzvf -"',{'connecting':'yes'})
+			shutit.pause_point('')
+		shutit.logout()
 		return True
 
 	def get_config(self, shutit):
