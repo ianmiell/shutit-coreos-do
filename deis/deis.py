d03a8a537840c9f0b4438f8f93fdd9bf3cbfd605
Ian Miell <ian.miell@gmail.com>
2015-01-25 22:16:57 +0000

    latest deis

--- deis/deis.py
+++ deis/deis.py
@@ -30,48 +30,52 @@ class deis(ShutItModule):
 		shutit.send('docl keys | grep -w ' + ssh_key_name)
 		ssh_key = shutit.send_and_get_output('docl keys | grep -w ' + ssh_key_name + r""" | sed 's/.*id: \([0-9]*\))/\1/'""")
 		ssh_key = ssh_key.strip()
-		output = shutit.send_and_get_output('./contrib/digitalocean/provision-do-cluster.sh nyc3 ' + ssh_key + ' 4GB | grep "^[0-9]"').split()
-		shutit.send('ls ~/.ssh && cat ~/.ssh/deis*')
+		output = shutit.send_and_get_output('./contrib/digitalocean/provision-do-cluster.sh nyc3 ' + ssh_key + ' 512MB | grep "^[0-9]"').split()
 		# delete the domain
+		shutit.send('curl -s -X DELETE -H "Content-Type: application/json" -H "Authorization: Bearer ' + token + '" "https://api.digitalocean.com/v2/domains/' + domain + '"')
+		# create the domain
+		shutit.send('curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ' + token + '''" -d '{"name":"''' + domain + '''","ip_address":"''' + output[0] + '''"}' "https://api.digitalocean.com/v2/domains/"''')
 		# set up the CNAME record name=* hostname=@
+		shutit.send('curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ' + token + '''" -d '{"type":"CNAME","name":"*","data":"@","priority":null,"port":null,"weight":null}' "https://api.digitalocean.com/v2/domains/''' + domain + '''/records"''')
 		# set up the 3 @ A records
 		# set up the 3 deis-1/2/3 A records
-		#shutit.send('''curl -s -X GET -H 'Content-Type: application/json' -u "${TOKEN}:" "https://api.digitalocean.com/v2/domains/"''' + domain + ''' > /tmp/domain_out''')
-		#curl -X POST -H 'Content-Type: application/json' -H 'Authorization: Bearer b7d03a6947b217efb6f3ec3bd3504582' -d '{"type":"A","name":"customdomainrecord.com","data":"162.10.66.0","priority":null,"port":null,"weight":null}' "https://api.digitalocean.com/v2/domains/digitaloceanisthebombdiggity.com/records"
-		#cf:            command = '''curl -s -X POST -H 'Content-Type: application/json' -H "Authorization: Bearer $TOKEN" -d '{"name":"coreos-''' + str(machine) + '''","region":"nyc3","size":"512mb","image":"coreos-stable","ssh_keys":["''' + ssh_key_id + '''"],"backups":false,"ipv6":true,"user_data":"''' + cloud_config + '''","private_networking":true}' "https://api.digitalocean.com/v2/droplets"'''
-
-
-		shutit.pause_point('''================================================================================
-Now go here: https://cloud.digitalocean.com/domains
-and add:
-- An entry for your domain name pointing at ''' + output[0] + ''' 
-- A wildcard CNAME record at your top-level domain, i.e. a CNAME record with * as the name, and @ as the canonical hostname
-- For each CoreOS machine created, an A-record that points to the TLD, i.e. an A-record named @, with the droplet's public IP address
-- For each CoreOS machine created, an A-record that points to the name, ie deis-1, deis-2, deis-3 so you can go to each server directly
-
-Created machines are:
-
-''' + output[0] + '\n' + output[1] + '\n' + output[2] + '\n' + '''
-
-The zone files will have the following entries in it:
-
-*   CNAME   @
-@   IN A    ''' + output[0] + '''
-@   IN A    ''' + output[1] + '''
-@   IN A    ''' + output[2] + '''
-
-For convenience, you can also set up DNS records for each node:
-
-deis-1   IN A    ''' + output[0] + '''
-deis-2   IN A    ''' + output[1] + '''
-deis-3   IN A    ''' + output[2] + '''
-
-See here if you need more info: https://www.digitalocean.com/community/tutorials/how-to-set-up-a-host-name-with-digitalocean
-================================================================================
-
-WHEN FINISHED, HIT "CRTL" THEN "]" AT THE SAME TIME TO CONTINUE
-
-================================================================================ ''')
+		count = 1
+		for addr in output:
+			shutit.send('''curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ''' + token + '''" -d '{"type":"A","name":"@","data":"''' + addr + '''","priority":null,"port":null,"weight":null}' "https://api.digitalocean.com/v2/domains/''' + domain + '''/records"''')
+			shutit.send('curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer ' + token + '''" -d '{"type":"A","name":"deis-''' + str(count) + '''","data":"''' + addr + '''","priority":null,"port":null,"weight":null}' "https://api.digitalocean.com/v2/domains/''' + domain + '''/records"''')
+			count += 1
+#		shutit.pause_point('''================================================================================
+#Now go here: https://cloud.digitalocean.com/domains
+#and add:
+#- An entry for your domain name pointing at ''' + output[0] + ''' 
+#- A wildcard CNAME record at your top-level domain, i.e. a CNAME record with * as the name, and @ as the canonical hostname
+#- For each CoreOS machine created, an A-record that points to the TLD, i.e. an A-record named @, with the droplet's public IP address
+#- For each CoreOS machine created, an A-record that points to the name, ie deis-1, deis-2, deis-3 so you can go to each server directly
+#
+#Created machines are:
+#
+#''' + output[0] + '\n' + output[1] + '\n' + output[2] + '\n' + '''
+#
+#The zone files will have the following entries in it:
+#
+#*   CNAME   @
+#@   IN A    ''' + output[0] + '''
+#@   IN A    ''' + output[1] + '''
+#@   IN A    ''' + output[2] + '''
+#
+#For convenience, you can also set up DNS records for each node:
+#
+#deis-1   IN A    ''' + output[0] + '''
+#deis-2   IN A    ''' + output[1] + '''
+#deis-3   IN A    ''' + output[2] + '''
+#
+#See here if you need more info: https://www.digitalocean.com/community/tutorials/how-to-set-up-a-host-name-with-digitalocean
+#================================================================================
+#
+#WHEN FINISHED, HIT "CRTL" THEN "]" AT THE SAME TIME TO CONTINUE
+#
+#================================================================================ ''')
+		shutit.pause_point('test domain')
 		shutit.multisend('scp -i /root/.ssh/' + ssh_key_name + ' /root/.ssh/' + ssh_key_name + ' core@' + output[0] + ':.ssh/',{'continue connecting':'yes'})
 		shutit.login(command='ssh -i /root/.ssh/' + ssh_key_name + ' core@' + output[0],expect=' ~ ')
 		shutit.send('chmod 0600 ~/.ssh/' + ssh_key_name)
